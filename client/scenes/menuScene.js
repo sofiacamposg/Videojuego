f6ad9ff70c7999c2b8d048d90436b13590d0cdba
sofiacamposg <a01277013@tec.mx>
2026-03-03 21:40:44 -0600

    Made different js for each screen

---
+++ client/scenes/menuScene.js
@@ -0,0 +1,74 @@
+// Mouse
+let mouseX = 0;
+let mouseY = 0;
+
+const buttonStart = {
+    x: 500,
+    y: 450,
+    text: "START"
+};
+
+const buttonSettings = {
+    x: 500,
+    y: 500,
+    text: "SETTINGS"
+};
+
+const buttonLogIn = {
+    x: 500,
+    y: 550,
+    text: "LOG IN"
+};
+
+// Imagen de fondo
+let backgroundImage = new Image();
+backgroundImage.src = "./assets/Portada.png";
+
+console.log(backgroundImage.src);
+function draw(ctx, canvas) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+
+    drawButton(ctx, buttonStart);
+    drawButton(ctx, buttonSettings);
+    drawButton(ctx, buttonLogIn);
+}
+
+function drawButton(ctx, button) {
+    ctx.font = "25px 'VT223'";
+    ctx.textAlign = "center";
+
+    const textWidth = ctx.measureText(button.text).width;
+    const textHeight = 30;
+
+    const left = button.x - textWidth / 2;
+    const right = button.x + textWidth / 2;
+    const top = button.y - textHeight;
+    const bottom = button.y;
+
+    const isHover =
+        mouseX > left &&
+        mouseX < right &&
+        mouseY > top &&
+        mouseY < bottom;
+
+    ctx.fillStyle = isHover ? "red" : "white";
+    ctx.fillText(button.text, button.x, button.y);
+
+    if (isHover) {
+        ctx.beginPath();
+        ctx.moveTo(left, button.y + 5);
+        ctx.lineTo(right, button.y + 5);
+        ctx.strokeStyle = "red";
+        ctx.lineWidth = 3;
+        ctx.stroke();
+    }
+}
+
+function handleMouseMove(event, canvas) {
+    const rect = canvas.getBoundingClientRect();
+    mouseX = event.clientX - rect.left;
+    mouseY = event.clientY - rect.top;
+}
+
+export { draw, handleMouseMove };
