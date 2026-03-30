6df6bd71cd7a584faef21b45f2fcbc2c1330ea05
0Angulo0 <angulosolisdaniela15@gmail.com>
2026-03-30 17:36:50 -0600

    Agregue los assets para caminata a la izquierda y salto, también la logica para que sepa cuando usar cual. SOLO PLAYER 1

--- client/objects/PlayerBase.js
+++ client/objects/PlayerBase.js
@@ -4,20 +4,25 @@ class PlayerBase extends AnimatedObject {
 
   constructor(position){
     super(position,160,160,"white","player",6)
+    this.direction = "right";  //default direction
   }
 
   update(){
-    // movimiento
-    if(this.isMoving){ //Si hay movimiento haz update de frame
-      this.updateAnimation(20);
+    if (!this.isOnGround) {
+      //case 1: jump
+      this.spriteImage = (this.direction === "right") ? this.spriteJumpRight : this.spriteJumpLeft;
+      this.frame = 0;  
+      if (this.spriteRect) this.spriteRect.x = 0;  //return to initial frame
+    } else if (this.isMoving) {
+      //case 2: walk
+      this.spriteImage = (this.direction === "right") ? this.spriteRight : this.spriteLeft;
+      this.updateAnimation(15);
+    } else {
+      //case 3: stay
+      this.spriteImage = (this.direction === "right") ? this.spriteRight : this.spriteLeft;
+      this.frame = 0;  //returns variable to 0
+      if (this.spriteRect) this.spriteRect.x = 0;  //returns image to initial frame
     }
-    else{
-      this.frame = 0;
-      if(this.spriteRect){ //si no se mueve quedate en el frame 0
-        this.spriteRect.x = 0;
-      }
-    }
-    
   }
 
   draw(ctx){
