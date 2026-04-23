d64ba04d30bb6eb779d7ad2eb00aac9b5138dede
0Angulo0 <angulosolisdaniela15@gmail.com>
2026-04-22 23:08:57 -0600

    actualice el código con las modificaciones que mencionaron los profes, aun no está termiando :)

--- server/db/gladiador_codigo2.sql
+++ server/db/gladiador_codigo2.sql
@@ -5,7 +5,7 @@ USE gladiator;
 -- Stores player account data, demographic/basic information, and general counters
 CREATE TABLE Player (
     player_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
+    name VARCHAR(30) NOT NULL,
     username VARCHAR(30) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     registration_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -54,7 +54,7 @@ INSERT INTO Level (level_number, target_time, description) VALUES
 (3, 30, 'Final battle of the colosseum');
 
 -- Table: Card
--- Stores cards and effect
+-- Stores cards, effect and details for the js code 
 CREATE TABLE Card (
     card_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
     card_name VARCHAR(40) NOT NULL UNIQUE,
@@ -135,13 +135,10 @@ CREATE TABLE SpecificLevel (
     CONSTRAINT chk_completion_time CHECK (completion_time >= 0),
     CONSTRAINT chk_remaining_hp CHECK (remaining_hp > 0),
     CONSTRAINT chk_fame_gained CHECK (fame_gained > 0),
-    CONSTRAINT chk_powerups_obtained CHECK (powerups_obtained IN (1, 2)),
-    CONSTRAINT chk_punishments_obtained CHECK (punishments_obtained IN (1, 2))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Table: Deck
--- Renamed from used card table
--- Stores which cards were used during a specific level
+-- Renamed from used card table, Stores which cards are on players deck
 CREATE TABLE Deck (
     deck_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     card_id SMALLINT UNSIGNED NOT NULL,
@@ -157,10 +154,24 @@ CREATE TABLE Deck (
     CONSTRAINT chk_quantity CHECK (quantity < 6)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- Table: Level Card
+-- Transition table to conect cards with specific level, cards earned during a run
+CREATE TABLE LevelCard (
+    level_card_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    card_id  SMALLINT UNSIGNED NOT NULL,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (level_card_id),
+    CONSTRAINT fk_players_deck
+        FOREIGN KEY (player_id) REFERENCES Player(player_id),
+)
+
 -- Table: Statistics
 -- Stores general game stats, login count, user activity, and admin log info
 CREATE TABLE Statistics (
-    statistics_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    statistics_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    username VARCHAR(30) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
     logins_count INT UNSIGNED NOT NULL DEFAULT 0,
     users_count INT UNSIGNED NOT NULL DEFAULT 0,
     user_movements_count INT UNSIGNED NOT NULL DEFAULT 0,
@@ -187,7 +198,6 @@ INSERT INTO Card (card_name, description, type, effect_from, effect_modifies, ef
 ('Imperial Decree Card', 'A punishment card that spawns more enemies', 4),
 ('Caesar Chains Card', 'A punishment card that disables jumping', 5),
 ('Jupiter Wrath Card', 'A punishment card that removes life instantly', 6);
-
 
 -- Dummy data for Enemy
 INSERT INTO Enemy (level_id, enemy_name, hp_start, speed_start, damage_start, enemy_type) VALUES
@@ -229,6 +239,7 @@ INSERT INTO Statistics (
 ) VALUES
 (25, 3, 140, 3, 2, 1, 'Initial dummy data loaded');
 
+----& VIEW
 -- View: general stats summary
 CREATE VIEW vw_general_statistics AS
 SELECT
@@ -249,199 +260,6 @@ SELECT
     p.total_wins,
     p.total_losses
 FROM Player p;
-
--- Stored Procedure: get all matches by player
-DELIMITER //
-CREATE PROCEDURE GetMatchesByPlayer(IN in_player_id SMALLINT UNSIGNED)
-BEGIN
-    SELECT *
-    FROM MatchGame
-    WHERE player_id = in_player_id;
-END //
-DELIMITER ;
-
--- Stored Procedure: insert a new match
-DELIMITER //
-CREATE PROCEDURE InsertMatch(
-    IN in_player_id SMALLINT UNSIGNED,
-    IN in_archetype_id SMALLINT UNSIGNED,
-    IN in_duration_seconds SMALLINT UNSIGNED,
-    IN in_level_reached TINYINT UNSIGNED,
-    IN in_final_fame SMALLINT UNSIGNED,
-    IN in_life TINYINT UNSIGNED,
-    IN in_result ENUM('WIN', 'LOSE')
-)
-BEGIN
-    INSERT INTO MatchGame (
-        player_id, archetype_id, end_time, duration_seconds,
-        level_reached, final_fame, life, result
-    )
-    VALUES (
-        in_player_id, in_archetype_id, NOW(), in_duration_seconds,
-        in_level_reached, in_final_fame, in_life, in_result
-    );
-END //
-DELIMITER ;
-
--- Trigger: update player counters after inserting a match
-DELIMITER //
-CREATE TRIGGER trg_after_insert_match
-AFTER INSERT ON MatchGame
-FOR EACH ROW
-BEGIN
-    UPDATE Player
-    SET total_runs = total_runs + 1,
-        total_wins = total_wins + IF(NEW.result = 'WIN', 1, 0),
-        total_losses = total_losses + IF(NEW.result = 'LOSE', 1, 0)
-    WHERE player_id = NEW.player_id;
-
-    UPDATE Statistics
-    SET matches_count = matches_count + 1,
-        wins_count = wins_count + IF(NEW.result = 'WIN', 1, 0),
-        losses_count = losses_count + IF(NEW.result = 'LOSE', 1, 0),
-        updated_at = CURRENT_TIMESTAMP
-    WHERE statistics_id = 1;
-END //
-DELIMITER ;
-
--- Trigger: update user count when a new player is inserted
-DELIMITER //
-CREATE TRIGGER trg_after_insert_player
-AFTER INSERT ON Player
-FOR EACH ROW
-BEGIN
-    UPDATE Statistics
-    SET users_count = users_count + 1,
-        updated_at = CURRENT_TIMESTAMP
-    WHERE statistics_id = 1;
-END //
-DELIMITER ;
-
--- Trigger: each time a card is registered as used in Deck, add 1 to user_movement_count
-DELIMITER //
-
-CREATE TRIGGER trg_after_insert_deck
-AFTER INSERT ON Deck
-FOR EACH ROW
-BEGIN
-    UPDATE Statistics
-    SET user_movements_count = user_movements_count + 1,
-        updated_at = CURRENT_TIMESTAMP
-    WHERE statistics_id = 1;
-END //
-
-DELIMITER ;
--- Trigger: Validate an effect is not terporary and permanent at the same time
-DELIMITER //
-
-CREATE TRIGGER trg_before_insert_temporaryeffect
-BEFORE INSERT ON TemporaryEffect
-FOR EACH ROW
-BEGIN
-    IF EXISTS (
-        SELECT 1
-        FROM PermanentEffect
-        WHERE effect_id = NEW.effect_id
-    ) THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'An effect cannot be both temporary and permanent.';
-    END IF;
-END //
-
-DELIMITER ;
-
--- Trigger: Validate an effect is not terporary and permanent at the same time
-DELIMITER //
-
-CREATE TRIGGER trg_before_insert_permanenteffect
-BEFORE INSERT ON PermanentEffect
-FOR EACH ROW
-BEGIN
-    IF EXISTS (
-        SELECT 1
-        FROM TemporaryEffect
-        WHERE effect_id = NEW.effect_id
-    ) THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'An effect cannot be both permanent and temporary.';
-    END IF;
-END //
-
-DELIMITER ;
-
--- STORED PROCEDURES
---Obbtains cards for effect type (POWER UP OR PUNISHMENT)
-DELIMITER //
-
-CREATE PROCEDURE GetCardsByType(IN in_effect_type ENUM('POWER_UP', 'PUNISHMENT'))
-BEGIN
-    SELECT
-        c.card_id,
-        c.card_name,
-        e.effect_name,
-        e.effect_type,
-        e.effect_value,
-        e.description
-    FROM Card c
-    INNER JOIN Effect e ON c.effect_id = e.effect_id
-    WHERE e.effect_type = in_effect_type;
-END //
-
-DELIMITER ;
-
--- Stored procedure to get a match summary, for our socre scene
-DELIMITER //
-
-CREATE PROCEDURE GetMatchSummary(IN in_match_id INT UNSIGNED)
-BEGIN
-    SELECT
-        m.match_id,
-        p.name AS player_name,
-        a.name AS archetype,
-        m.duration_seconds,
-        m.level_reached,
-        m.final_fame,
-        m.life,
-        m.result,
-        COUNT(sl.specific_level_id) AS levels_played,
-        SUM(sl.fame_gained) AS total_fame_from_levels,
-        SUM(sl.powerups_obtained) AS total_powerups,
-        SUM(sl.punishments_obtained) AS total_punishments
-    FROM MatchGame m
-    INNER JOIN Player p ON m.player_id = p.player_id
-    INNER JOIN Archetype a ON m.archetype_id = a.archetype_id
-    LEFT JOIN SpecificLevel sl ON m.match_id = sl.match_id
-    WHERE m.match_id = in_match_id
-    GROUP BY m.match_id;
-END //
-
-DELIMITER ;
-
---Stored procedure to get used cards in a match
-DELIMITER //
-
-CREATE PROCEDURE GetCardsUsedInMatch(IN in_match_id INT UNSIGNED)
-BEGIN
-    SELECT
-        m.match_id,
-        sl.specific_level_id,
-        l.level_number,
-        c.card_name,
-        e.effect_name,
-        e.effect_type,
-        d.use_time,
-        d.effect_duration
-    FROM MatchGame m
-    INNER JOIN SpecificLevel sl ON m.match_id = sl.match_id
-    INNER JOIN Level l ON sl.level_id = l.level_id
-    INNER JOIN Deck d ON sl.specific_level_id = d.specific_level_id
-    INNER JOIN Card c ON d.card_id = c.card_id
-    INNER JOIN Effect e ON c.effect_id = e.effect_id
-    WHERE m.match_id = in_match_id
-    ORDER BY l.level_number, d.use_time;
-END //
-
-DELIMITER ;
 
 -- VIEW FOR OUL LIFE STATS
 -- This shows life, fame and current level
@@ -542,3 +360,198 @@ INNER JOIN MatchGame m ON sl.match_id = m.match_id
 INNER JOIN Player p ON m.player_id = p.player_id
 INNER JOIN Card c ON d.card_id = c.card_id
 GROUP BY p.username, c.card_name;
+
+
+-------& TRIGGER
+-- Trigger: update player counters after inserting a match
+DELIMITER //
+CREATE TRIGGER trg_after_insert_match
+AFTER INSERT ON MatchGame
+FOR EACH ROW
+BEGIN
+    UPDATE Player
+    SET total_runs = total_runs + 1,
+        total_wins = total_wins + IF(NEW.result = 'WIN', 1, 0),
+        total_losses = total_losses + IF(NEW.result = 'LOSE', 1, 0)
+    WHERE player_id = NEW.player_id;
+
+    UPDATE Statistics
+    SET matches_count = matches_count + 1,
+        wins_count = wins_count + IF(NEW.result = 'WIN', 1, 0),
+        losses_count = losses_count + IF(NEW.result = 'LOSE', 1, 0),
+        updated_at = CURRENT_TIMESTAMP
+    WHERE statistics_id = 1;
+END //
+DELIMITER ;
+
+-- Trigger: update user count when a new player is inserted
+DELIMITER //
+CREATE TRIGGER trg_after_insert_player
+AFTER INSERT ON Player
+FOR EACH ROW
+BEGIN
+    UPDATE Statistics
+    SET users_count = users_count + 1,
+        updated_at = CURRENT_TIMESTAMP
+    WHERE statistics_id = 1;
+END //
+DELIMITER ;
+
+-- Trigger: each time a card is registered as used in Deck, add 1 to user_movement_count
+DELIMITER //
+
+CREATE TRIGGER trg_after_insert_deck
+AFTER INSERT ON Deck
+FOR EACH ROW
+BEGIN
+    UPDATE Statistics
+    SET user_movements_count = user_movements_count + 1,
+        updated_at = CURRENT_TIMESTAMP
+    WHERE statistics_id = 1;
+END //
+
+DELIMITER ;
+-- Trigger: Validate an effect is not terporary and permanent at the same time
+DELIMITER //
+
+CREATE TRIGGER trg_before_insert_temporaryeffect
+BEFORE INSERT ON TemporaryEffect
+FOR EACH ROW
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM PermanentEffect
+        WHERE effect_id = NEW.effect_id
+    ) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'An effect cannot be both temporary and permanent.';
+    END IF;
+END //
+
+DELIMITER ;
+
+-- Trigger: Validate an effect is not terporary and permanent at the same time
+DELIMITER //
+
+CREATE TRIGGER trg_before_insert_permanenteffect
+BEFORE INSERT ON PermanentEffect
+FOR EACH ROW
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM TemporaryEffect
+        WHERE effect_id = NEW.effect_id
+    ) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'An effect cannot be both permanent and temporary.';
+    END IF;
+END //
+
+DELIMITER ;
+
+--& STORED PROCEDURES
+-- Stored Procedure: get all matches by player
+DELIMITER //
+CREATE PROCEDURE GetMatchesByPlayer(IN in_player_id SMALLINT UNSIGNED)
+BEGIN
+    SELECT *
+    FROM MatchGame
+    WHERE player_id = in_player_id;
+END //
+DELIMITER ;
+
+-- Stored Procedure: insert a new match
+DELIMITER //
+CREATE PROCEDURE InsertMatch(
+    IN in_player_id SMALLINT UNSIGNED,
+    IN in_archetype_id SMALLINT UNSIGNED,
+    IN in_duration_seconds SMALLINT UNSIGNED,
+    IN in_level_reached TINYINT UNSIGNED,
+    IN in_final_fame SMALLINT UNSIGNED,
+    IN in_life TINYINT UNSIGNED,
+    IN in_result ENUM('WIN', 'LOSE')
+)
+BEGIN
+    INSERT INTO MatchGame (
+        player_id, archetype_id, end_time, duration_seconds,
+        level_reached, final_fame, life, result
+    )
+    VALUES (
+        in_player_id, in_archetype_id, NOW(), in_duration_seconds,
+        in_level_reached, in_final_fame, in_life, in_result
+    );
+END //
+DELIMITER ;
+
+--Obbtains cards for effect type (POWER UP OR PUNISHMENT)
+DELIMITER //
+
+CREATE PROCEDURE GetCardsByType(IN in_effect_type ENUM('POWER_UP', 'PUNISHMENT'))
+BEGIN
+    SELECT
+        c.card_id,
+        c.card_name,
+        e.effect_name,
+        e.effect_type,
+        e.effect_value,
+        e.description
+    FROM Card c
+    INNER JOIN Effect e ON c.effect_id = e.effect_id
+    WHERE e.effect_type = in_effect_type;
+END //
+
+DELIMITER ;
+
+-- Stored procedure to get a match summary, for our socre scene
+DELIMITER //
+
+CREATE PROCEDURE GetMatchSummary(IN in_match_id INT UNSIGNED)
+BEGIN
+    SELECT
+        m.match_id,
+        p.name AS player_name,
+        a.name AS archetype,
+        m.duration_seconds,
+        m.level_reached,
+        m.final_fame,
+        m.life,
+        m.result,
+        COUNT(sl.specific_level_id) AS levels_played,
+        SUM(sl.fame_gained) AS total_fame_from_levels,
+        SUM(sl.powerups_obtained) AS total_powerups,
+        SUM(sl.punishments_obtained) AS total_punishments
+    FROM MatchGame m
+    INNER JOIN Player p ON m.player_id = p.player_id
+    INNER JOIN Archetype a ON m.archetype_id = a.archetype_id
+    LEFT JOIN SpecificLevel sl ON m.match_id = sl.match_id
+    WHERE m.match_id = in_match_id
+    GROUP BY m.match_id;
+END //
+
+DELIMITER ;
+
+--Stored procedure to get used cards in a match
+DELIMITER //
+
+CREATE PROCEDURE GetCardsUsedInMatch(IN in_match_id INT UNSIGNED)
+BEGIN
+    SELECT
+        m.match_id,
+        sl.specific_level_id,
+        l.level_number,
+        c.card_name,
+        e.effect_name,
+        e.effect_type,
+        d.use_time,
+        d.effect_duration
+    FROM MatchGame m
+    INNER JOIN SpecificLevel sl ON m.match_id = sl.match_id
+    INNER JOIN Level l ON sl.level_id = l.level_id
+    INNER JOIN Deck d ON sl.specific_level_id = d.specific_level_id
+    INNER JOIN Card c ON d.card_id = c.card_id
+    INNER JOIN Effect e ON c.effect_id = e.effect_id
+    WHERE m.match_id = in_match_id
+    ORDER BY l.level_number, d.use_time;
+END //
+
+DELIMITER ;
